abf6ba671b0bd88b667c084a73c9d6448214d589
Philip James <phildini@phildini.net>
2024-06-12 00:21:56 -0700

    Fix umami again

--- plugins/umami.py
+++ plugins/umami.py
@@ -4,5 +4,5 @@ from datasette import hookimpl
 @hookimpl
 def extra_body_script():
     return {
-        "script": '<script defer src="https://analytics.civic.band/sunshine" data-website-id="43ac0bfd-5789-4d27-9c9b-409d11e092d1"></script>'
+        "script": '</script><script defer src="https://analytics.civic.band/sunshine" data-website-id="43ac0bfd-5789-4d27-9c9b-409d11e092d1">'
     }
